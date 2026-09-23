fef8a190a902feff37f0a3bda6158df7d4e6fb0b
rubelgamer360email-beep <rubelgamer360email@gmail.com>
2026-09-24 02:42:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, render_template_string, send_from_directory
 import os
 import random
 import base64
